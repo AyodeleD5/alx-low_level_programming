e662e77b19961f89bb7c8c601cd18ba0bdec4c1d
AyodeleD5 <ayodeled550@gmail.com>
2023-10-10 21:45:24 -0700

    Structures

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Dealing with Structures and Typedof
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+/**
+ * struct dog - structure of dog
+ * @name: the name of the dog
+ * @age: the dog's age
+ * @owner: dog owner
+*/
+#ifndef DOG_H
+#define DOG_H
+
+struct dog {
+
+	char *name;
+	float age;
+	char * owner;
+};
+
+#endif
