f9146176f5983ed6f49bfc070722ae00482d9d70
AyodeleD5 <ayodeled550@gmail.com>
2023-10-19 01:01:50 -0700

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,37 +7,26 @@
  * @s2: second string
  * Return: 0
  */
-int wildcmp_no_wildcards(char *s1, char *s2);
-int wildcmp(char *s1, char *s2)
-{
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (0);
-	}
-	return (wildcmp_no_wildcards(s1, s2) || wildcmp(s1 + 1, s2 + 1));
-}
 
-/**
- * wildcmp_no_wildcards - compares two strings
- * @s1: first string
- * @s2: second string
- * Return: 0
- */
-int wildcmp_no_wildcards(char *s1, char *s2)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (1);
-	}
-	
-	if (*s1 != '*' && *s1 != *s2)
-	{
-		return (0);
-	}
-	
-	if (*s1 == '*')
-	{
-		return (wildcmp_no_wildcards(s1 + 1, s2));
-	}
-	return (wildcmp_no_wildcards(s1 + 1, s2 + 1));
+int wildcmp(char *s1, char *s2) {
+    // If both strings are empty, they are considered identical.
+    if (*s1 == '\0' && *s2 == '\0') {
+        return 1;
+    }
+
+    // If the current characters match or if s2 contains a '*', we have two options:
+    if (*s2 == '*' || *s1 == *s2) {
+        // 1. Move to the next character in s1 and s2 (match * to an empty string).
+        if (*s2 == '*' && wildcmp(s1, s2 + 1)) {
+            return 1;
+        }
+
+        // 2. Move to the next character in s1 (match the current characters).
+        if (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1)) {
+            return 1;
+        }
+    }
+
+    // If none of the above conditions are met, the strings are not identical.
+    return 0;
 }
