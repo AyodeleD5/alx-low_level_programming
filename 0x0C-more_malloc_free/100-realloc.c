d4abb72dea186a75499a0db1ad0ffc12b81e9fc1
AyodeleD5 <ayodeled550@gmail.com>
2023-10-20 15:43:22 -0700

    push it

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,40 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to memory previously allocated
- * @old_size: size in bytes of allocated space for ptr
- * @new_size: new size in bytes for the new memory block
- * Return: 0
+ * _realloc -  reallocates a memory block using malloc and free
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
-	unsigned int min_size;
+	char *clone, *relloc;
+	unsigned int i;
 
-	new_ptr = malloc(new_size);
-
-	if (new_ptr == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr == NULL || new_size > old_size)
-		return (new_ptr);
-
+	if (ptr != NULL)
+	clone = ptr;
+	else
+	{ return (malloc(new_size)); }
 	if (new_size == old_size)
-		return (ptr);
-
-	min_size = (old_size < new_size) ? old_size : new_size;
-	memcpy(new_ptr, ptr, min_size);
+	return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{ free(ptr);
+	return (0); }
+	relloc = malloc(new_size);
+	if (relloc == NULL)
+	return (0);
+	for (i = 0; i < (old_size || i < new_size); i++)
+	{
+		*(relloc + i) = clone[i];
+	}
 	free(ptr);
-
-	return (new_ptr);
+return (relloc);
 }
