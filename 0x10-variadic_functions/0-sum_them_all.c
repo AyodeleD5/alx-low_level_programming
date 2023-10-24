38797a3243d19c9b0095f7f0e8087d734fafa8cc
AyodeleD5 <ayodeled550@gmail.com>
2023-10-24 01:21:14 -0700

    Correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,22 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
-	unsigned int i;
-	int sum = 0;
 
-	if (n == 0)
-		return (0);
+	va_list args;  // Create a va_list to work with the arguments.
+   	int sum = 0;
 
-	va_start(valist, n);
+    	if (n == 0) {
+        	return 0;  // If n is 0, return 0 as per the prototype.
+	}
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
+   	va_start(args, n);  // Initialize the va_list with the arguments.
 
-	va_end(valist);
+    	// Loop through the parameters and accumulate the sum.
+    	for (unsigned int i = 0; i < n; i++) {
+        	sum += va_arg(args, int);
+    	}
 
-	return (sum);
+    	va_end(args);  // Clean up the va_list.
+
+    	return sum;  // Return the calculated sum.
 }
