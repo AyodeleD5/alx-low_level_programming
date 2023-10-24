ffd3d016e57540d52a660ebc36f6977a33979896
AyodeleD5 <ayodeled550@gmail.com>
2023-10-24 01:04:43 -0700

    Correction

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-/**
- * main - adds positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
- */
-int main(int argc, char *argv[])
-{
-	long int i, num, sum = 0;
+#include <stdbool.h>
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (*argv[i] >= ':' && *argv[i] <= '}')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				num = atoi(argv[i]);
-				sum += num;
-			}
-		}
-		printf("%ld\n", sum);
-	}
-	return (0);
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("0\n");
+        return 0;
+    }
+
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++) {
+        char *arg = argv[i];
+
+        // Check if the argument contains only digits
+        bool isPositive = true;
+        for (int j = 0; arg[j] != '\0'; j++) {
+            if (arg[j] < '0' || arg[j] > '9') {
+                isPositive = false;
+                break;
+            }
+        }
+
+        if (!isPositive) {
+            printf("Error\n");
+            return 1;
+        }
+
+        int num = atoi(arg);
+        if (num <= 0) {
+            printf("Error\n");
+            return 1;
+        }
+
+        sum += num;
+    }
+
+    printf("%d\n", sum);
+    return 0;
 }
+
